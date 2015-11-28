eb3d6650682458f3ae914e3ef883dde0fdf2dd94
Vitor Fernandes <vmlf01@gmail.com>
2015-11-28 16:16:08 +0000

    add webpack config file comments

--- 01-static-site/webpack.config.js
+++ 01-static-site/webpack.config.js
@@ -1,9 +1,15 @@
 var webpack = require("webpack");
 
+// webpack config file is just a node module,
+// so we need to export a config object with
+// the settings
 module.exports = {
+  // set the entry point for webpack compilation
   entry: {
     app: "./app.js"
   },
+
+  // set the output filename for the generated bundle
   output: {
     filename: "bundle.js"
   }
--- 02-static-site-adv/webpack.config.js
+++ 02-static-site-adv/webpack.config.js
@@ -1,24 +1,31 @@
 var path = require('path');
 var webpack = require('webpack');
+
+// extract-text is used to extract CSS definitions into separate CSS file
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// indexhtml is used to enable index.html as an entry point
 var IndexHtmlPlugin = require('indexhtml-webpack-plugin');
 
+// setup css target file name
 var cssExtractPlugin = new ExtractTextPlugin('css/bundle-[contenthash:16].css');
 
 module.exports = {
-  debug: true,
+  // root folder for all reference paths
   context: path.join(__dirname, 'src'),
-  //resolve: {
-  //  fallback: path.join(__dirname, 'node_modules')
-  //},
-  //resolveLoader: {
-  //  root: path.join(__dirname, 'node_modules')
-  //},
+
+  // entry points for webpack to analyse
   entry: {
     'index.html': './index.html',
     'js/app.js': './js/app.js'
   },
+
+  // loaders to use to process files
   module:{
+
+    // lint all js files before compilation process starts
+    // jshint config bellow will stop webpack if there is
+    // any js problems
     preLoaders: [
       {
         test: /\.js$/,
@@ -27,42 +34,56 @@ module.exports = {
       },
     ],
     loaders: [
+
+      // css -> style -> text-extract all CSS files;
+      // css loads css file contents and identifies dependencies;
+      // style will link the CSS generated bundle into the html file;
+      // cssExtractPlugin will generate a CSS bundle file;
+      // publicPath option is needed because we are putting the CSS file
+      // into a 'css' subfolder.
       {
         test: /\.css$/,
         loader: cssExtractPlugin.extract('style', 'css', { publicPath: '../' })
       },
+
+      // use url loader for font files
+      // url will inline font file contents if size is less than 'limit' value
       {
         test: /\.(woff|woff2|eot|ttf|svg)(\?v=.*)?$/i,
         loaders: [ 'url?name=font/[name]-[hash].[ext]&limit=10000' ]
       },
+
+      // use html loader to load index.html and include dependencies
+      // from 'link' and 'img' tags
       {
         test: /index\.html$/,
         loader: 'html?attrs=link:href img:src'
       },
+
+      // expose jquery module as 'jQuery' global variable
+      // because bootstrap needs it to work
       {
         test: require.resolve('jquery'),
         loader: 'expose?$!expose?jQuery'
       }
     ]
   },
+
+  // register plug-ins
   plugins: [
     cssExtractPlugin,
     new IndexHtmlPlugin('index.html', 'index.html')
   ],
+
+  // set output folder and filename
   output: {
     path: './dist',
     filename: '[name]'
   },
 
-  // more options in the optional jshint object
   jshint: {
-    // jshint errors are displayed by default as warnings
-    // set emitErrors to true to display them as errors
-    emitErrors: false,
-
-    // jshint to not interrupt the compilation
-    // if you want any file with jshint errors to fail
-    // set failOnHint to true
+    // set jshint to interrupt the compilation
+    // if any file errors
     failOnHint: true
   }
 };
